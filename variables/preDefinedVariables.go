b4edde1ba209540f85f44b90234bdbd82e484c1f
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2018-10-07 15:32:06 +0700

    Variables 🍎

---
+++ variables/preDefinedVariables.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	/*
+		Underscore (_) is a predefined variable that can be used to hold unused values.
+		You could say this variable is a waste basket.
+	*/
+
+	// Single variable declaration
+	_ = "Learn Golang is funny"
+
+	// Multi variable declaration
+	name, _ = "Muhammad Harits Syaifulloh", "Learn Golang is funny"
+}
